4edf6ef499ce61558902d942d7422585981ad0f2
tiannian <dtiannian@gmail.com>
2025-06-19 02:24:54 +0800

    add scripts

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 - Hash based
 - Pederson Commitment based
+
+## Run
+
+```bash
+./scripts/snarkjs_setup.sh utxochecker 18
+./scripts/snarkjs_proof.sh utxochecker utxo_basic
+```
--- scripts/snarkjs_setup.sh
+++ scripts/snarkjs_setup.sh
@@ -3,9 +3,17 @@
 CIRCOM_NAME=$1
 PTAU_SIZE=$2
 
+mkdir -p target/circuits
+mkdir -p target/ptaus
+
 circom circuits/${CIRCOM_NAME}.circom --r1cs --wasm -o target/circuits/ -l ../circomlib/circuits || exit 1
 
 cd target/circuits || exit 1
+
+if [ ! -f ../ptaus/powersOfTau28_hez_final_${PTAU_SIZE}.ptau ]; then
+    echo "PTAU file not found, downloading..."
+    curl -o ../ptaus/powersOfTau28_hez_final_${PTAU_SIZE}.ptau https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_${PTAU_SIZE}.ptau || exit 1
+fi
 
 snarkjs groth16 setup ${CIRCOM_NAME}.r1cs ../ptaus/powersOfTau28_hez_final_${PTAU_SIZE}.ptau ${CIRCOM_NAME}_0000.zkey || exit 1
 snarkjs zkey contribute ${CIRCOM_NAME}_0000.zkey ${CIRCOM_NAME}_0001.zkey --name="First contribution" -v || exit 1
