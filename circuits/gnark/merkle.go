7b712b7a92f7754a26d430d673ae19e311e9bd63
tiannian <dtiannian@gmail.com>
2025-06-25 20:12:38 +0800

    try to test merkle tree

--- circuits/gnark/merkle.go
+++ circuits/gnark/merkle.go
@@ -1,13 +1,58 @@
 package circuits
 
 import (
+	"fmt"
+
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	poseidonbn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/poseidon2"
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/hash/poseidon2"
+)
+
+const (
+	MAX_MERKLE_DEPTH = 24
 )
 
 type MerkleGadget struct {
 	Path      []frontend.Variable `gnark:"path"`
 	Direction []frontend.Variable `gnark:"direction"`
+}
+
+func NewMerkleGadget() *MerkleGadget {
+	return &MerkleGadget{
+		Path:      make([]frontend.Variable, MAX_MERKLE_DEPTH*3),
+		Direction: make([]frontend.Variable, MAX_MERKLE_DEPTH*3),
+	}
+}
+
+func (gadget *MerkleGadget) Verify(api frontend.API) (frontend.Variable, error) {
+	if len(gadget.Path) != MAX_MERKLE_DEPTH*3 || len(gadget.Direction) != MAX_MERKLE_DEPTH*3 {
+		return 0, fmt.Errorf("invalid path or direction length")
+	}
+
+	for i := 0; i < MAX_MERKLE_DEPTH-1; i++ {
+		hasher, err := poseidon2.NewMerkleDamgardHasher(api)
+		if err != nil {
+			return 0, err
+		}
+
+		hasher.Write(gadget.Path[i*3], gadget.Path[i*3+1], gadget.Path[i*3+2])
+		hash := hasher.Sum()
+
+		gadget.Path[i*3] = api.Mul(gadget.Path[i*3], hash)
+		gadget.Path[i*3+1] = api.Mul(gadget.Path[i*3+1], hash)
+		gadget.Path[i*3+2] = api.Mul(gadget.Path[i*3+2], hash)
+	}
+
+	hasher, err := poseidon2.NewMerkleDamgardHasher(api)
+	if err != nil {
+		return 0, err
+	}
+
+	hasher.Write(gadget.Path[MAX_MERKLE_DEPTH*3-3], gadget.Path[MAX_MERKLE_DEPTH*3-2], gadget.Path[MAX_MERKLE_DEPTH*3-1])
+	hash := hasher.Sum()
+
+	return hash, nil
 }
 
 type MerkleProofNode struct {
@@ -18,10 +63,83 @@ type MerkleProofNode struct {
 }
 
 type MerkleProof struct {
-	Root fr.Element
 	Path []MerkleProofNode
 }
 
-func (proof *MerkleProof) Verify() error {
+func (proof *MerkleProof) Verify(root fr.Element) error {
+	if len(proof.Path) != MAX_MERKLE_DEPTH {
+		return fmt.Errorf("invalid proof length")
+	}
+
+	for i := 0; i < MAX_MERKLE_DEPTH-1; i++ {
+		hasher := poseidonbn254.NewMerkleDamgardHasher()
+
+		leftBytes := proof.Path[i].Left.Bytes()
+		middleBytes := proof.Path[i].Middle.Bytes()
+		rightBytes := proof.Path[i].Right.Bytes()
+
+		hasher.Write(leftBytes[:])
+		hasher.Write(middleBytes[:])
+		hasher.Write(rightBytes[:])
+
+		hash := hasher.Sum(make([]byte, 32))
+		hashElement := fr.NewElement(0)
+		hashElement.SetBytes(hash)
+
+		if proof.Path[i].Direction == 0 {
+			proof.Path[i+1].Left = hashElement
+		} else if proof.Path[i].Direction == 1 {
+			proof.Path[i+1].Middle = hashElement
+		} else if proof.Path[i].Direction == 2 {
+			proof.Path[i+1].Right = hashElement
+		}
+	}
+
+	hasher := poseidonbn254.NewMerkleDamgardHasher()
+	leftBytes := proof.Path[MAX_MERKLE_DEPTH-1].Left.Bytes()
+	middleBytes := proof.Path[MAX_MERKLE_DEPTH-1].Middle.Bytes()
+	rightBytes := proof.Path[MAX_MERKLE_DEPTH-1].Right.Bytes()
+
+	hasher.Write(leftBytes[:])
+	hasher.Write(middleBytes[:])
+	hasher.Write(rightBytes[:])
+
+	hash := hasher.Sum(make([]byte, 32))
+	hashElement := fr.NewElement(0)
+	hashElement.SetBytes(hash)
+
+	if hashElement != root {
+		return fmt.Errorf("invalid root")
+	}
+
 	return nil
+}
+
+func (proof *MerkleProof) ToGadget() (*MerkleGadget, error) {
+	path := make([]frontend.Variable, len(proof.Path)*3)
+	direction := make([]frontend.Variable, len(proof.Path)*3)
+
+	for i := range proof.Path {
+		path[i*3] = proof.Path[i].Left
+		path[i*3+1] = proof.Path[i].Middle
+		path[i*3+2] = proof.Path[i].Right
+
+		if proof.Path[i].Direction == 0 {
+			direction[i*3] = 1
+			direction[i*3+1] = 0
+			direction[i*3+2] = 0
+		} else if proof.Path[i].Direction == 1 {
+			direction[i*3] = 0
+			direction[i*3+1] = 1
+			direction[i*3+2] = 0
+		} else if proof.Path[i].Direction == 2 {
+			direction[i*3] = 0
+			direction[i*3+1] = 0
+			direction[i*3+2] = 1
+		} else {
+			return nil, fmt.Errorf("invalid direction")
+		}
+	}
+
+	return &MerkleGadget{Path: path, Direction: direction}, nil
 }
---
+++ circuits/gnark/merkle_test.go
@@ -0,0 +1,91 @@
+package circuits_test
+
+import (
+	circuits "hide-pay/circuits/gnark"
+	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	poseidonbn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/poseidon2"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func buildPureLeftMerkleProof() (*circuits.MerkleProof, fr.Element, error) {
+	proof := circuits.MerkleProof{
+		Path: make([]circuits.MerkleProofNode, circuits.MAX_MERKLE_DEPTH),
+	}
+
+	current := fr.NewElement(100)
+
+	for i := 0; i < circuits.MAX_MERKLE_DEPTH; i++ {
+		proof.Path[i].Left = current
+		proof.Path[i].Middle = fr.NewElement(0)
+		proof.Path[i].Right = fr.NewElement(0)
+		proof.Path[i].Direction = 0
+
+		hasher := poseidonbn254.NewMerkleDamgardHasher()
+
+		leftBytes := proof.Path[i].Left.Bytes()
+		middleBytes := proof.Path[i].Middle.Bytes()
+		rightBytes := proof.Path[i].Right.Bytes()
+
+		hasher.Write(leftBytes[:])
+		hasher.Write(middleBytes[:])
+		hasher.Write(rightBytes[:])
+
+		hash := hasher.Sum(make([]byte, 32))
+		hashElement := fr.NewElement(0)
+		hashElement.SetBytes(hash)
+
+		current = hashElement
+	}
+
+	return &proof, current, nil
+}
+
+func TestMerkleProof(t *testing.T) {
+	proof, root, err := buildPureLeftMerkleProof()
+	assert.NoError(t, err)
+
+	err = proof.Verify(root)
+	assert.NoError(t, err)
+}
+
+type MerkleCircuit struct {
+	circuits.MerkleGadget
+	Root frontend.Variable `gnark:"root,public"`
+}
+
+func (c *MerkleCircuit) Define(api frontend.API) error {
+	root, err := c.MerkleGadget.Verify(api)
+	if err != nil {
+		return err
+	}
+
+	api.AssertIsEqual(root, c.Root)
+	return nil
+}
+
+func TestMerkleCircuit(t *testing.T) {
+	proof, root, err := buildPureLeftMerkleProof()
+	assert.NoError(t, err)
+
+	circuit := MerkleCircuit{
+		MerkleGadget: *circuits.NewMerkleGadget(),
+	}
+
+	gadget, err := proof.ToGadget()
+	assert.NoError(t, err)
+
+	witness := MerkleCircuit{
+		MerkleGadget: *gadget,
+		Root:         root,
+	}
+
+	assert := test.NewAssert(t)
+	options := test.WithCurves(ecc.BN254)
+
+	assert.ProverSucceeded(&circuit, &witness, options)
+}
