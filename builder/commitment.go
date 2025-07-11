916025a8fdadf354cda48391fc41381b8fe0cdcd
tiannian <dtiannian@gmail.com>
2025-07-11 10:48:22 +0800

    update commitment

--- builder/commitment.go
+++ builder/commitment.go
@@ -6,12 +6,19 @@ import (
 
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr/poseidon2"
+	twistededwardbn254 "github.com/consensys/gnark-crypto/ecc/bn254/twistededwards"
 )
 
 type Commitment struct {
-	Asset    fr.Element
-	Amount   fr.Element
-	Blinding fr.Element
+	Asset         fr.Element
+	Amount        fr.Element
+	OwnerPubKey   twistededwardbn254.PointAffine
+	SpentAddress  fr.Element
+	ViewPubKey    twistededwardbn254.PointAffine
+	AuditPubKey   twistededwardbn254.PointAffine
+	FreezeAddress fr.Element
+	FreezeFlag    fr.Element
+	Blinding      fr.Element
 }
 
 func (commitment *Commitment) ToGadget() *circuits.CommitmentGadget {
@@ -23,23 +30,41 @@ func (commitment *Commitment) ToGadget() *circuits.CommitmentGadget {
 }
 
 func (commitment *Commitment) String() string {
-	return fmt.Sprintf("Commitment(Asset: %s, Amount: %s, Blinding: %s)", commitment.Asset.Text(10), commitment.Amount.Text(10), commitment.Blinding.Text(10))
+	return fmt.Sprintf("Commitment(Asset: %s, Amount: %s, Blinding: %s,)", commitment.Asset.Text(10), commitment.Amount.Text(10), commitment.Blinding.Text(10))
 }
 
 func (commitment *Commitment) Compute() fr.Element {
 	hasher := poseidon2.NewMerkleDamgardHasher()
 
-	asset_bytes := commitment.Asset.Bytes()
-	amount_bytes := commitment.Amount.Bytes()
-	blinding_bytes := commitment.Blinding.Bytes()
+	assetBytes := commitment.Asset.Bytes()
+	amountBytes := commitment.Amount.Bytes()
+	ownerPubKeyXBytes := commitment.OwnerPubKey.X.Bytes()
+	ownerPubKeyYBytes := commitment.OwnerPubKey.Y.Bytes()
+	spentAddressBytes := commitment.SpentAddress.Bytes()
+	viewPubKeyXBytes := commitment.ViewPubKey.X.Bytes()
+	viewPubKeyYBytes := commitment.ViewPubKey.Y.Bytes()
+	auditPubKeyXBytes := commitment.AuditPubKey.X.Bytes()
+	auditPubKeyYBytes := commitment.AuditPubKey.Y.Bytes()
+	freezeAddressBytes := commitment.FreezeAddress.Bytes()
+	freezeFlagBytes := commitment.FreezeFlag.Bytes()
+	blindingBytes := commitment.Blinding.Bytes()
 
-	hasher.Write(asset_bytes[:])
-	hasher.Write(amount_bytes[:])
+	hasher.Write(assetBytes[:])
+	hasher.Write(amountBytes[:])
+	hasher.Write(ownerPubKeyXBytes[:])
+	hasher.Write(ownerPubKeyYBytes[:])
+	hasher.Write(spentAddressBytes[:])
+	hasher.Write(viewPubKeyXBytes[:])
+	hasher.Write(viewPubKeyYBytes[:])
+	hasher.Write(auditPubKeyXBytes[:])
+	hasher.Write(auditPubKeyYBytes[:])
+	hasher.Write(freezeAddressBytes[:])
+	hasher.Write(freezeFlagBytes[:])
 
-	res_bytes := hasher.Sum(blinding_bytes[:])
+	resBytes := hasher.Sum(blindingBytes[:])
 
 	res := fr.Element{}
-	res.Unmarshal(res_bytes)
+	res.Unmarshal(resBytes)
 
 	return res
 }
--- builder/commitment_test.go
+++ builder/commitment_test.go
@@ -2,6 +2,8 @@ package builder_test
 
 import (
 	"hide-pay/builder"
+	"hide-pay/utils"
+	"math/big"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
@@ -9,11 +11,19 @@ import (
 )
 
 func TestCommitment_Compute(t *testing.T) {
+	ownerPubKey := utils.BuildPublicKey(*big.NewInt(12345))
+	viewPubKey := utils.BuildPublicKey(*big.NewInt(54321))
+	auditPubKey := utils.BuildPublicKey(*big.NewInt(11111))
+
 	// Test basic commitment computation
 	commitment := &builder.Commitment{
-		Asset:    fr.NewElement(12345),
-		Amount:   fr.NewElement(67890),
-		Blinding: fr.NewElement(11111),
+		Asset:         fr.NewElement(12345),
+		Amount:        fr.NewElement(67890),
+		OwnerPubKey:   ownerPubKey,
+		ViewPubKey:    viewPubKey,
+		AuditPubKey:   auditPubKey,
+		FreezeAddress: fr.NewElement(11121314),
+		Blinding:      fr.NewElement(11111),
 	}
 
 	result := commitment.Compute()
@@ -25,29 +35,49 @@ func TestCommitment_Compute(t *testing.T) {
 }
 
 func TestCommitment_Compute_DifferentInputs(t *testing.T) {
+	ownerPubKey := utils.BuildPublicKey(*big.NewInt(12345))
+	viewPubKey := utils.BuildPublicKey(*big.NewInt(54321))
+	auditPubKey := utils.BuildPublicKey(*big.NewInt(11111))
+
 	// Test that different inputs produce different commitments
 	commitment1 := &builder.Commitment{
-		Asset:    fr.NewElement(12345),
-		Amount:   fr.NewElement(67890),
-		Blinding: fr.NewElement(11111),
+		Asset:         fr.NewElement(12345),
+		Amount:        fr.NewElement(67890),
+		OwnerPubKey:   ownerPubKey,
+		ViewPubKey:    viewPubKey,
+		AuditPubKey:   auditPubKey,
+		FreezeAddress: fr.NewElement(11121314),
+		Blinding:      fr.NewElement(11111),
 	}
 
 	commitment2 := &builder.Commitment{
-		Asset:    fr.NewElement(54321), // Different asset
-		Amount:   fr.NewElement(67890),
-		Blinding: fr.NewElement(11111),
+		Asset:         fr.NewElement(54321), // Different asset
+		Amount:        fr.NewElement(67890),
+		OwnerPubKey:   ownerPubKey,
+		ViewPubKey:    viewPubKey,
+		AuditPubKey:   auditPubKey,
+		FreezeAddress: fr.NewElement(11121314),
+		Blinding:      fr.NewElement(11111),
 	}
 
 	commitment3 := &builder.Commitment{
-		Asset:    fr.NewElement(12345),
-		Amount:   fr.NewElement(98765), // Different amount
-		Blinding: fr.NewElement(11111),
+		Asset:         fr.NewElement(12345),
+		Amount:        fr.NewElement(98765), // Different amount
+		OwnerPubKey:   ownerPubKey,
+		ViewPubKey:    viewPubKey,
+		AuditPubKey:   auditPubKey,
+		FreezeAddress: fr.NewElement(11121314),
+		Blinding:      fr.NewElement(11111),
 	}
 
 	commitment4 := &builder.Commitment{
-		Asset:    fr.NewElement(12345),
-		Amount:   fr.NewElement(67890),
-		Blinding: fr.NewElement(22222), // Different blinding factor
+		Asset:         fr.NewElement(12345),
+		Amount:        fr.NewElement(67890),
+		OwnerPubKey:   ownerPubKey,
+		ViewPubKey:    viewPubKey,
+		AuditPubKey:   auditPubKey,
+		FreezeAddress: fr.NewElement(11121314),
+		Blinding:      fr.NewElement(22222), // Different blinding factor
 	}
 
 	result1 := commitment1.Compute()
@@ -65,11 +95,19 @@ func TestCommitment_Compute_DifferentInputs(t *testing.T) {
 }
 
 func TestCommitment_Compute_Deterministic(t *testing.T) {
+	ownerPubKey := utils.BuildPublicKey(*big.NewInt(12345))
+	viewPubKey := utils.BuildPublicKey(*big.NewInt(54321))
+	auditPubKey := utils.BuildPublicKey(*big.NewInt(11111))
+
 	// Test that same inputs produce same commitment (deterministic)
 	commitment := &builder.Commitment{
-		Asset:    fr.NewElement(12345),
-		Amount:   fr.NewElement(67890),
-		Blinding: fr.NewElement(11111),
+		Asset:         fr.NewElement(12345),
+		Amount:        fr.NewElement(67890),
+		OwnerPubKey:   ownerPubKey,
+		ViewPubKey:    viewPubKey,
+		AuditPubKey:   auditPubKey,
+		FreezeAddress: fr.NewElement(11121314),
+		Blinding:      fr.NewElement(11111),
 	}
 
 	result1 := commitment.Compute()
@@ -82,11 +120,19 @@ func TestCommitment_Compute_Deterministic(t *testing.T) {
 }
 
 func TestCommitment_Compute_ZeroValues(t *testing.T) {
+	ownerPubKey := utils.BuildPublicKey(*big.NewInt(0))
+	viewPubKey := utils.BuildPublicKey(*big.NewInt(0))
+	auditPubKey := utils.BuildPublicKey(*big.NewInt(0))
+
 	// Test zero value inputs
 	commitment := &builder.Commitment{
-		Asset:    fr.NewElement(0),
-		Amount:   fr.NewElement(0),
-		Blinding: fr.NewElement(0),
+		Asset:         fr.NewElement(0),
+		Amount:        fr.NewElement(0),
+		OwnerPubKey:   ownerPubKey,
+		ViewPubKey:    viewPubKey,
+		AuditPubKey:   auditPubKey,
+		FreezeAddress: fr.NewElement(0),
+		Blinding:      fr.NewElement(0),
 	}
 
 	result := commitment.Compute()
@@ -94,11 +140,19 @@ func TestCommitment_Compute_ZeroValues(t *testing.T) {
 }
 
 func TestCommitment_Compute_LargeValues(t *testing.T) {
+	ownerPubKey := utils.BuildPublicKey(*big.NewInt(0x7FFFFFFFFFFFFFFF))
+	viewPubKey := utils.BuildPublicKey(*big.NewInt(0x7FFFFFFFFFFFFFFF))
+	auditPubKey := utils.BuildPublicKey(*big.NewInt(0x7FFFFFFFFFFFFFFF))
+
 	// Test large values
 	commitment := &builder.Commitment{
-		Asset:    fr.NewElement(0xFFFFFFFFFFFFFFFF),
-		Amount:   fr.NewElement(0xFFFFFFFFFFFFFFFF),
-		Blinding: fr.NewElement(0xFFFFFFFFFFFFFFFF),
+		Asset:         fr.NewElement(0xFFFFFFFFFFFFFFFF),
+		Amount:        fr.NewElement(0xFFFFFFFFFFFFFFFF),
+		OwnerPubKey:   ownerPubKey,
+		ViewPubKey:    viewPubKey,
+		AuditPubKey:   auditPubKey,
+		FreezeAddress: fr.NewElement(0xFFFFFFFFFFFFFFFF),
+		Blinding:      fr.NewElement(0xFFFFFFFFFFFFFFFF),
 	}
 
 	result := commitment.Compute()
@@ -106,11 +160,19 @@ func TestCommitment_Compute_LargeValues(t *testing.T) {
 }
 
 func TestCommitment_ToCircuit_Consistency(t *testing.T) {
+	ownerPubKey := utils.BuildPublicKey(*big.NewInt(12345))
+	viewPubKey := utils.BuildPublicKey(*big.NewInt(54321))
+	auditPubKey := utils.BuildPublicKey(*big.NewInt(11111))
+
 	// Test consistency of multiple conversions
 	commitment := &builder.Commitment{
-		Asset:    fr.NewElement(12345),
-		Amount:   fr.NewElement(67890),
-		Blinding: fr.NewElement(11111),
+		Asset:         fr.NewElement(12345),
+		Amount:        fr.NewElement(67890),
+		OwnerPubKey:   ownerPubKey,
+		ViewPubKey:    viewPubKey,
+		AuditPubKey:   auditPubKey,
+		FreezeAddress: fr.NewElement(11121314),
+		Blinding:      fr.NewElement(11111),
 	}
 
 	witness1 := commitment.ToGadget()
@@ -130,49 +192,81 @@ func TestCommitment_ToCircuit_Consistency(t *testing.T) {
 func TestCommitment_Compute_EdgeCases(t *testing.T) {
 	// Test edge cases
 	testCases := []struct {
-		name     string
-		asset    uint64
-		amount   uint64
-		blinding uint64
+		name          string
+		asset         uint64
+		amount        uint64
+		ownerPubKey   uint64
+		viewPubKey    uint64
+		auditPubKey   uint64
+		freezeAddress uint64
+		blinding      uint64
 	}{
 		{
-			name:     "All zeros",
-			asset:    0,
-			amount:   0,
-			blinding: 0,
+			name:          "All zeros",
+			asset:         0,
+			amount:        0,
+			ownerPubKey:   0,
+			viewPubKey:    0,
+			auditPubKey:   0,
+			freezeAddress: 0,
+			blinding:      0,
 		},
 		{
-			name:     "All ones",
-			asset:    1,
-			amount:   1,
-			blinding: 1,
+			name:          "All ones",
+			asset:         1,
+			amount:        1,
+			ownerPubKey:   1,
+			viewPubKey:    1,
+			auditPubKey:   1,
+			freezeAddress: 1,
+			blinding:      1,
 		},
 		{
-			name:     "Mixed values",
-			asset:    0,
-			amount:   1,
-			blinding: 0,
+			name:          "Mixed values",
+			asset:         0,
+			amount:        1,
+			ownerPubKey:   0,
+			viewPubKey:    1,
+			auditPubKey:   0,
+			freezeAddress: 1,
+			blinding:      0,
 		},
 		{
-			name:     "Large asset, small others",
-			asset:    0xFFFFFFFFFFFFFFFF,
-			amount:   1,
-			blinding: 1,
+			name:          "Large asset, small others",
+			asset:         0xFFFFFFFFFFFFFFFF,
+			amount:        1,
+			ownerPubKey:   1,
+			viewPubKey:    1,
+			auditPubKey:   1,
+			freezeAddress: 1,
+			blinding:      1,
 		},
 		{
-			name:     "Small asset, large others",
-			asset:    1,
-			amount:   0xFFFFFFFFFFFFFFFF,
-			blinding: 0xFFFFFFFFFFFFFFFF,
+			name:          "Small asset, large others",
+			asset:         1,
+			amount:        0xFFFFFFFFFFFFFFFF,
+			ownerPubKey:   0x7FFFFFFFFFFFFFFF,
+			viewPubKey:    0x7FFFFFFFFFFFFFFF,
+			auditPubKey:   0x7FFFFFFFFFFFFFFF,
+			freezeAddress: 0xFFFFFFFFFFFFFFFF,
+			blinding:      0xFFFFFFFFFFFFFFFF,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			ownerPubKey := utils.BuildPublicKey(*big.NewInt(int64(tc.ownerPubKey)))
+			viewPubKey := utils.BuildPublicKey(*big.NewInt(int64(tc.viewPubKey)))
+			auditPubKey := utils.BuildPublicKey(*big.NewInt(int64(tc.auditPubKey)))
+
 			commitment := &builder.Commitment{
-				Asset:    fr.NewElement(tc.asset),
-				Amount:   fr.NewElement(tc.amount),
-				Blinding: fr.NewElement(tc.blinding),
+				Asset:         fr.NewElement(tc.asset),
+				Amount:        fr.NewElement(tc.amount),
+				OwnerPubKey:   ownerPubKey,
+				ViewPubKey:    viewPubKey,
+				AuditPubKey:   auditPubKey,
+				FreezeAddress: fr.NewElement(tc.freezeAddress),
+				Blinding:      fr.NewElement(tc.blinding),
 			}
 
 			result := commitment.Compute()
